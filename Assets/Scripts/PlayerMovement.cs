4615fb7c61b49842711fea1f85ae2bd9c211e9d9
Emil Z <istarnightstudios@gmail.com>
2024-12-13 15:57:04 -0800

    Camera Independant of Player

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,21 +5,27 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public Rigidbody rb;
-    public float moveSpeed;
-    public float sprintSpeed;
+
+    //Variables
+    public float speedMoving;
 
     public PlayerControlls controls;
+    
+    //References
+    public Rigidbody rb;
+    [SerializeField] private Transform lookPivot;
 
-
+    //Input Action References
     private InputAction move;
 
-    Vector3 moveDirection;
 
     private void Awake()
     {
         controls = new PlayerControlls();
+        move = controls.Player.Move;
     }
+
+    #region enable/disable movement
 
     private void OnEnable()
     {
@@ -34,6 +40,7 @@ public class PlayerMovement : MonoBehaviour
         Debug.Log("Moving Disabled");
 
     }
+    #endregion 
 
     // Start is called before the first frame update
     void Start()
@@ -41,14 +48,33 @@ public class PlayerMovement : MonoBehaviour
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        moveDirection = move.ReadValue<Vector2>();
-    }
+
 
     private void FixedUpdate()
     {
-        rb.linearVelocity = new Vector3(moveDirection.x * moveSpeed, 0, moveDirection.y * moveSpeed);
+        Vector3 forward = lookPivot.forward; 
+        forward.y = 0f;
+        forward.Normalize();
+
+        Vector3 right = lookPivot.right;
+        right.y = 0f;
+        right.Normalize();
+
+        Vector2 inputDirection = move.ReadValue<Vector2>();
+        Vector3 moveDirection = new Vector3(inputDirection.x, 0, inputDirection.y).normalized;
+
+
+
+        rb.linearVelocity = moveDirection * speedMoving + new Vector3(0, rb.linearVelocity.y, 0);
     }
+
+
+
+    //Custom Functions
+
+    private void CachePivotDirections()
+    {
+        
+    }
+
 }
